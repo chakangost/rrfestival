7f41ebdc38108ca123e1bde5e15901817c81dfd4
chakangost <chakangost@naver.com>
2017-03-20 20:08:15 +0900

    first

---
+++ app/src/main/java/kr/rrcoporation/rrfestival/festival/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.rrcoporation.rrfestival.festival;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
