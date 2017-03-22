670ad92fda50e783d3597752ae96f2535112b7c7
chakangost <chakangost@naver.com>
2017-03-22 19:31:25 +0900

    dd

--- app/src/main/java/kr/rrcoporation/rrfestival/festival/activity/FragmentContainerActivity.java
+++ app/src/main/java/kr/rrcoporation/rrfestival/festival/activity/FragmentContainerActivity.java
@@ -1,6 +1,5 @@
 package kr.rrcoporation.rrfestival.festival.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
--- app/src/main/java/kr/rrcoporation/rrfestival/festival/fragment/FavFragment.java
+++ app/src/main/java/kr/rrcoporation/rrfestival/festival/fragment/FavFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-
 import kr.rrcoporation.rrfestival.festival.R;
 
 public class FavFragment extends CommonFragment {
