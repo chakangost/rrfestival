77f1cf8f24ddff775d4193ffd00451d404b73d25
chakangost <chakangost@naver.com>
2017-03-22 20:26:51 +0900

    다음 맵 적용

--- app/src/main/java/kr/rrcoporation/rrfestival/festival/fragment/MapFragment.java
+++ app/src/main/java/kr/rrcoporation/rrfestival/festival/fragment/MapFragment.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-
+import net.daum.mf.map.api.MapView;
 import kr.rrcoporation.rrfestival.festival.R;
 import kr.rrcoporation.rrfestival.festival.callback.FragmentContainerBottomCallback;
 
@@ -24,6 +24,10 @@ public class MapFragment extends CommonFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootLayout = (LinearLayout) inflater.inflate(R.layout.fragment_map, null);
+        MapView mapView = new MapView(getActivity());
+        mapView.setDaumMapApiKey(getString(R.string.daum_api_key));
+        ViewGroup mapViewContainer = (ViewGroup) rootLayout.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
         return rootLayout;
     }
 
